8f719ea2751b127394e7ca626a530fa652c40f5c
Nagarjuna devireddy <nagarjunadevireddy007@gmail.com>
2021-09-27 16:01:31 +0530

    javascript part

--- app.js
+++ app.js
@@ -4,4 +4,28 @@ var welcomeMessage = "This script works"+ username;
 alert(welcomeMessage); */
 
 var btnTranslate = document.querySelector("#btn-translate");
-var txtInput =  document.querySelector(#txt-input);
+var txtInput =  document.querySelector("#txt-input");
+var outputDiv = document.querySelector ("#outputDiv");
+
+var serverURL = "https://api.funtranslations.com/translate/minion.json"
+
+function getTranslationURL(text){
+    return serverURL+"?"+"text="+text
+}
+
+function clickHandler() {
+    var inputText = txtInput.value; //input
+
+    // calling the server to process
+    fetch( getTranslationURL(inputText))
+    .then((response) => response.json())
+    .then((json) => {
+        var translationText = json.contents.translated;
+       outputDiv.innerText= translationText;
+
+    });
+}
+btnTranslate.addEventListener("click", clickHandler);
+
+
+
